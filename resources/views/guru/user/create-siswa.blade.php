b1075773ad246c4d34cd795458d677ed215bd928
Ryan Charles Wijaya <ryancharlesrcw@gmail.com>
2022-06-28 01:46:13 +0700

    Bug Fix

--- resources/views/guru/user/create-siswa.blade.php
+++ resources/views/guru/user/create-siswa.blade.php
@@ -70,7 +70,7 @@
                     <div class="mb-3">
                         <label
                             class="form-label text-capitalize">
-                            Nama Sekolah
+                            Nama Sekolah Asal
                         </label>
                         <input
                             type="text"
